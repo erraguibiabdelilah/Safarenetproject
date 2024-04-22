e30fb0895fbc542d0dbb683ce2ffbc1f11de26ff
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-04-22 18:47:41 +0100

    commit 18:47

--- AppFront/src/app/app.component.spec.ts
+++ AppFront/src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -14,27 +14,24 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<<< HEAD:AppFront/src/app/app.component.spec.ts
   it(`should have as title 'AppFront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('AppFront');
-========
+  });
+
   it(`should have the 'Font' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    app.title = 'Font'; // Set the title to 'Font'
     expect(app.title).toEqual('Font');
->>>>>>>> 685d440aea307017f862c6121c4a06be5ee33161:Font/src/app/app.component.spec.ts
   });
 
-  it('should render title', () => {
+  it('should render the title in the h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<<< HEAD:AppFront/src/app/app.component.spec.ts
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AppFront');
-========
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Font');
->>>>>>>> 685d440aea307017f862c6121c4a06be5ee33161:Font/src/app/app.component.spec.ts
   });
 });
